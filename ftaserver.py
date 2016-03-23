221b68709fbf889d1d863510e1a31f8e11d9aecb
cdgallahue <cdgallahue@gmail.com>
2016-03-23 16:47:26 -0400

    Most current version of client and server

--- ftaserver.py
+++ ftaserver.py
@@ -1,15 +1,15 @@
 from RTP import *
 from sys import *
 
-def main():
-    if len(sys.argv) != 3:
-        print "Requires the port number to listen on, as well as the window size."
-        return -1
-    else:
-        port = int(sys.argv[1])
-        window = int(sys.argv[2])
-        receipt = None
-        RTPserv = RTP(None, port, port, True, window)
-        while True:
-            receipt = RTPserv.listen()
-        
+
+if len(sys.argv) != 3:
+    print("Requires the port number to listen on, as well as the window size.")
+    exit()
+else:
+    port = int(sys.argv[1])
+    window = int(sys.argv[2])
+    receipt = None
+    RTPserv = RTP(None, port, port, True, window)
+    while True:
+        receipt = RTPserv.listen()
+            
